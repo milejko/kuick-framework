7e4d8f85afa9066f355c757fdebd473aad909454
Mariusz Milejko <mariusz@milejko.pl>
2024-11-20 13:47:59 +0100

    fixed installer

--- src/Composer/ComposerInstaller.php
+++ src/Composer/ComposerInstaller.php
@@ -17,22 +17,22 @@ use Composer\Script\Event;
  */
 class ComposerInstaller
 {
-    private const KUICK_PATH = BASE_PATH . '/vendor/kuick/framework';
+    private const KUICK_PATH =  '/vendor/kuick/framework';
     private const INDEX_FILE = '/public/index.php';
     private const CONSOLE_FILE = '/bin/console';
     private const ETC_FILE_LOCATIONS = [
         '/etc/*.config.php',
-        '/etc/ui/*.routes.php',
-        '/etc/ui/*.commands.php',
-        '/etc/di/*.di.php',
+        '/etc/routes/*.actions.php',
+        '/etc/routes/*.commands.php',
+        '/etc/di/sample.di.php',
     ];
-    private const TMP_DIR = BASE_PATH . '/var/tmp';
+    private const TMP_DIR = '/var/tmp';
     private const SYS_DIRS = ['etc', 'etc/di', 'etc/routes', 'public', 'bin'];
 
     protected static function initAutoload(Event $event)
     {
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
-        define('BASE_PATH', realpath($vendorDir . '/../'));
+        define('BASE_PATH', realpath(dirname($vendorDir)));
         require $vendorDir . '/autoload.php';
     }
 
@@ -50,9 +50,9 @@ class ComposerInstaller
 
     protected static function createSysDirs()
     {
-        !file_exists(self::TMP_DIR) ?
-            mkdir(self::TMP_DIR, 0777, true) :
-            chmod(self::TMP_DIR, 0777);
+        !file_exists(BASE_PATH . self::TMP_DIR) ?
+            mkdir(BASE_PATH . self::TMP_DIR, 0777, true) :
+            chmod(BASE_PATH . self::TMP_DIR, 0777);
         foreach (self::SYS_DIRS as $dir) {
             !file_exists(BASE_PATH . DIRECTORY_SEPARATOR . $dir) ?
                 mkdir(BASE_PATH . DIRECTORY_SEPARATOR . $dir, 0755, true) :
@@ -62,19 +62,19 @@ class ComposerInstaller
 
     protected static function copyDistributionFiles()
     {
-        if (!file_exists(self::KUICK_PATH)) {
+        if (!file_exists(BASE_PATH . self::KUICK_PATH)) {
             return;
         }
-        copy(self::KUICK_PATH . self::INDEX_FILE, BASE_PATH . self::INDEX_FILE);
-        copy(self::KUICK_PATH . self::CONSOLE_FILE, BASE_PATH . self::CONSOLE_FILE);
+        copy(BASE_PATH . self::KUICK_PATH . self::INDEX_FILE, BASE_PATH . self::INDEX_FILE);
+        copy(BASE_PATH . self::KUICK_PATH . self::CONSOLE_FILE, BASE_PATH . self::CONSOLE_FILE);
         chmod(BASE_PATH . self::CONSOLE_FILE, 0755);
         foreach (self::ETC_FILE_LOCATIONS as $etcFileLocation) {
-            foreach (glob(BASE_PATH . $etcFileLocation) as $etcFilePath) {
-                $localEtcFileName = str_replace(self::KUICK_PATH, BASE_PATH, $etcFilePath);
+            foreach (glob(BASE_PATH . self::KUICK_PATH . $etcFileLocation) as $etcFilePath) {
+                $localEtcFileName = str_replace(BASE_PATH . self::KUICK_PATH, BASE_PATH, $etcFilePath);
                 if (file_exists($localEtcFileName)) {
                     continue;
                 }
-                copy($etcFileLocation, $localEtcFileName);
+                copy($etcFilePath, $localEtcFileName);
             }
         }
     }
