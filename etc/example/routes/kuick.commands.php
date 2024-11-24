5ab82f6b091ab4b11de2ae5462da49b32175343d
Mariusz Milejko <mariusz@milejko.pl>
2024-11-24 23:50:36 +0100

    example config fix

--- etc/example/routes/kuick.commands.php
+++ etc/example/routes/kuick.commands.php
@@ -1,6 +1,6 @@
 <?php
 
-use Kuick\Example\UI\Console\HelloCommand;
+use Kuick\Example\Console\HelloCommand;
 
 return [
     //you probably want to comment this one out, or remove this file entirely
